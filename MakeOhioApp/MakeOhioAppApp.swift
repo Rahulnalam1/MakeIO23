c2f39980a4fc23cd09a130e470e225c46f609fd5
Pranav-Chati <pchati.hack@gmail.com>
2023-03-04 18:55:12 -0500

    Initial Commit

---
+++ MakeOhioApp/MakeOhioAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MakeOhioAppApp.swift
+//  MakeOhioApp
+//
+//  Created by Pranav Chati on 3/4/23.
+//
+
+import SwiftUI
+
+@main
+struct MakeOhioAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
